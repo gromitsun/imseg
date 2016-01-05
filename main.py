7131d9abfbe6b0394bf0cba3382456afa376d3c5
gromitsun <wps_2003@hotmail.com>
2016-01-04 19:46:36 -0600

    Added time and multiple file functions.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
+import os
 import numpy as np
+from time import time
+import datetime
 from imseg.segment import read_input, ImSeg, proc_slice, dump_dict, dump_file
 
 try:
@@ -8,6 +11,9 @@ except IndexError:
     print('Error: expect input settings file!')
     sys.exit(-1)
 
+print('Starting program ...\n Time now is {}'.format(datetime.datetime.now()))
+
+# # Read and process input files
 print('Reading settings file %s ...' % settings_file)
 settings = read_input(settings_file)
 print('Reading parameters file %s ...' % settings['paras_file'])
@@ -26,31 +32,44 @@ data_slice = settings['data_slice']
 for i, x in enumerate(data_slice):
     data_slice[i] = proc_slice(x)
 
-print('Reading data file %s ...' % settings['path2data'])
-im = np.fromfile(settings['path2data'], dtype=settings['dtype']).reshape(settings['data_shape'])[data_slice]
-
-print('Initializing ImSeg object ...')
-seg = ImSeg(im, **paras)
-
-if 'continue' in settings:
-    seg.load(settings['continue'],
-             iter_count=settings.get('iter_count'),
-             thresholds=settings.get('thresholds'))
+if type(settings['path2data']) is not list:
+    data_file_list = [settings['path2data']]
 else:
-    seg.initialize()
+    data_file_list = settings['path2data']
 
-niter_plot = settings['niter_plot']
-niter_out = settings['niter_out']
-niter = settings['niter']
+for data_file in data_file_list:
+    outsubdir = os.path.splitext(os.path.basename(data_file))[0]
+    outdir = os.path.join(settings['outdir'], outsubdir)
+    # Read data file
+    print('Reading data file %s ...' % data_file)
+    im = np.fromfile(data_file, dtype=settings['dtype']).reshape(settings['data_shape'])[data_slice]
 
-plot_slice = settings['plot_slice']
-for i, x in enumerate(plot_slice):
-    plot_slice[i] = proc_slice(x)
+    print('Initializing ImSeg object ...')
+    seg = ImSeg(im, **paras)
 
-while seg.iter_count < niter:
-    seg.iterate(niter=1)
-    if (seg.iter_count % niter_out) == 0:
-        seg.save(prefix=settings['outdir'] + 'sdf_iter_', fmt='bin')
-    if (seg.iter_count % niter_plot) == 0:
-        seg.plot(outdir=settings['outdir'], im_slice=plot_slice)
-print('All done!')
+    if 'continue' in settings:
+        seg.load(settings['continue'],
+                 iter_count=settings.get('iter_count'),
+                 thresholds=settings.get('thresholds'))
+    else:
+        seg.initialize()
+
+    niter_plot = settings['niter_plot']
+    niter_out = settings['niter_out']
+    niter = settings['niter']
+
+    plot_slice = settings['plot_slice']
+    for i, x in enumerate(plot_slice):
+        plot_slice[i] = proc_slice(x)
+
+    while seg.iter_count < niter:
+        start = time()
+        seg.iterate(niter=1)
+        print('Completed 1 iteration in {} seconds.'.format(time()-start))
+        if (seg.iter_count % niter_out) == 0:
+            seg.save(prefix=outdir + 'sdf_iter_', fmt='bin')
+        if (seg.iter_count % niter_plot) == 0:
+            seg.plot(outdir=outdir, im_slice=plot_slice)
+
+print('Program finished!')
+print('Time now is {}'.format(datetime.datetime.now()))
